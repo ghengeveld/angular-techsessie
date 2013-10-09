0ce488f066c45d92f3d2bc78d810e6be6e517233
Gert Hengeveld <gert@finalist.com>
2013-10-09 12:49:53 +0200

    Basic hourtracker

--- app/scripts/controllers/hours-controller.js
+++ app/scripts/controllers/hours-controller.js
@@ -2,5 +2,10 @@
 
 angular.module('angularTechsessieApp')
   .controller('HoursController', function ($scope) {
+    $scope.hoursList = [];
 
+    $scope.addHours = function(hours) {
+      $scope.hoursList.push(hours);
+      $scope.hours = {};
+    }
   });
