dbb4231fa2d66501db02970dbc2a5999bcd0693e
Gert Hengeveld <gert@finalist.com>
2013-10-09 11:38:06 +0200

    Clean Yeoman-Angular installation

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularTechsessieApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
