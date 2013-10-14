7d14315ea76437653d190eba9aa108e15c3262ec
Gert Hengeveld <gert@finalist.com>
2013-10-14 18:20:00 +0200

    Implement column sorting

--- app/scripts/controllers/hours-controller.js
+++ app/scripts/controllers/hours-controller.js
@@ -31,6 +31,28 @@ angular.module('angularTechsessieApp')
 
     $scope.ordering = ['date', 'customer.name'];
 
+    $scope.orderBy = function(property) {
+      var asc = $scope.ordering.indexOf(property);
+      if (asc > 0) {
+        $scope.ordering.splice(asc, 1);
+        $scope.ordering.unshift('-' + property);
+        return;
+      } else if (asc === 0) {
+        $scope.ordering[0] = '-' + property;
+        return;
+      }
+      var desc = $scope.ordering.indexOf('-' + property);
+      if (desc > 0) {
+        $scope.ordering.splice(desc, 1);
+        $scope.ordering.unshift(property);
+        return;
+      } else if (desc === 0) {
+        $scope.ordering[0] = property;
+        return;
+      }
+      $scope.ordering.unshift(property);
+    };
+
     $scope.addHours = function(hours) {
       $scope.hoursList.push(hours);
       $scope.hours = {};
