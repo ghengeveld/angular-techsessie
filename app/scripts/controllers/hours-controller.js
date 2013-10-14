64dfb5cae806c97b69ac58ff4751b189df49c540
Gert Hengeveld <gert@finalist.com>
2013-10-14 08:35:21 +0200

    Add some base data

--- app/scripts/controllers/hours-controller.js
+++ app/scripts/controllers/hours-controller.js
@@ -2,10 +2,36 @@
 
 angular.module('angularTechsessieApp')
   .controller('HoursController', function ($scope) {
-    $scope.hoursList = [];
+
+    $scope.customers = [
+      { name: 'Finalist' },
+      { name: 'METRI' },
+      { name: 'Kennisnet' },
+      { name: 'VSP' },
+      { name: 'MOOG' },
+      { name: 'Amsterdam Museum' }
+    ];
+
+    $scope.hoursList = [{
+      date: '2013-10-15',
+      hours: 3,
+      customer: { name: 'Finalist' },
+      description: 'Techsessie AngularJS'
+    }, {
+      date: '2013-10-15',
+      hours: 1,
+      customer: { name: 'METRI' },
+      description: 'Implementeren easter-egg'
+    }, {
+      date: '2013-10-15',
+      hours: 2,
+      customer: { name: 'Finalist' },
+      description: 'Tafelvoetbal'
+    }];
 
     $scope.addHours = function(hours) {
       $scope.hoursList.push(hours);
       $scope.hours = {};
-    }
+    };
+
   });
